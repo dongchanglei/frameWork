8d9141e34acea33f9f1e8527a987205fe060d2ba
dongchanglei <dcllovelan@163.com>
2021-09-27 23:38:12 +0800

    module test

--- custom-spring/src/main/java/com/dcl/CustomSpring.java
+++ custom-spring/src/main/java/com/dcl/CustomSpring.java
@@ -1,4 +1,15 @@
 package com.dcl;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CustomSpring {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomSpring.class, args);
+
+
+    }
 }
